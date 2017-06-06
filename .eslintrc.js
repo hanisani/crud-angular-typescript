3ae1503a9057ecd20660c3d1d0a6b47b9cf64e9f
hanisani <faheemahmad@gmail.com>
2017-06-06 13:44:05 +0800

    commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "no-use-before-define": 0,
+    }    
+};
